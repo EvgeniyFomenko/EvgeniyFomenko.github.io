b06cce5ddded7ddc1ecf4d67e7c9d4bc7d326e4a
Evgeny Fomenko <gant26@rambler.ru>
2022-11-29 11:43:03 +0500

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/i.jpg') {
+      myImage.setAttribute ('src','images/we.jpg');
+    } else {
+      myImage.setAttribute ('src','images/i.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Добрый день меня зовут, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Добрый день меня зовут, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
